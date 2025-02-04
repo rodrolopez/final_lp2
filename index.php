4f22292b1baf8ca2f776bbba6f87f67805d13fe9
Rodrigo_Lopez <rodrilopez01@gmail.com>
2025-02-04 18:34:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi página</title>
+</head>
+<body>
+    <h1>¡Hola, mundo!</h1>
+</body>
+<form action="db.php" method="post">
+    <button type="submit">Ejecutar script</button>
+</form>
+</html>
